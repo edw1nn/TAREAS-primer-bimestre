215d19e55012fc2a58ddf63ad3545a697d3b9ad5
edw1nn <133687944+edw1nn@users.noreply.github.com>
2024-03-06 15:58:36 -0600

    Add files via upload

---
+++ EJERCICIOS C#/ejercicio de vector 3/ejercicio de vector 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Conversor de Kilómetros a Metros, Yardas y Varas");
+        Console.WriteLine("-----------------------------------------------");
+        Console.Write("Ingrese la cantidad en kilómetros: ");
+
+        double kilometros = Convert.ToDouble(Console.ReadLine());   
+        
+        double metros = kilometros * 1000;    
+        double yardas = kilometros * 1093.61;
+        double varas = kilometros * 1196.308;
+        
+        Console.WriteLine("\nResultados:");
+        Console.WriteLine("{0} kilómetros equivalen a {1} metros", kilometros, metros);
+        Console.WriteLine("{0} kilómetros equivalen a {1} yardas", kilometros, yardas);
+        Console.WriteLine("{0} kilómetros equivalen a {1} varas", kilometros, varas);
+        Console.ReadLine(); 
+    }
+}
